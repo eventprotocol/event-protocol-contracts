587ea7951dda403e4ea70d9a2b8514699016f580
Keshik <keshik6@gmail.com>
2018-07-13 17:07:43 +0800

    ERC223 receiver struct added

--- contracts/ERC223ReceivingContract.sol
+++ contracts/ERC223ReceivingContract.sol
@@ -11,6 +11,29 @@ pragma solidity ^0.4.24;
 /// @title Interface for ERC223 Receiving Contract
 /// @author keshik
 /// @dev The contract is a wrapper for ERC223 contracts which recieve funds
-interface ERC223ReceivingContract {
-    function tokenFallback(address _from, uint _value, bytes _data) external;
+contract ERC223ReceivingContract {
+
+    struct TKN {
+     address sender;
+     uint256 value;
+     bytes data;
+     bytes4 sig;
+    }
+
+    function tokenFallback(address _from, uint256 _value, bytes _data) public pure {
+      TKN memory tkn;
+      tkn.sender = _from;
+      tkn.value = _value;
+      tkn.data = _data;
+      uint32 u = uint32(_data[3]) + (uint32(_data[2]) << 8) + (uint32(_data[1]) << 16) + (uint32(_data[0]) << 24);
+      tkn.sig = bytes4(u);
+    /* tkn variable is analogue of msg variable of Ether transaction
+    *  tkn.sender is person who initiated this token transaction   (analogue of msg.sender)
+    *  tkn.value the number of tokens that were sent   (analogue of msg.value)
+    *  tkn.data is data of token transaction   (analogue of msg.data)
+    *  tkn.sig is 4 bytes signature of function
+    *  if data of token transaction is a function execution
+    */
+    }
+
 }
